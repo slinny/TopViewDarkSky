97ecd9f3c9fb9566d050b0e5b7be9af0c561b4b6
slinny <siranli411@gmail.com>
2020-02-09 21:33:09 -0500

    added locationservice

--- app/src/main/java/com/example/android/topviewdarksky/MainActivity.java
+++ app/src/main/java/com/example/android/topviewdarksky/MainActivity.java
@@ -1,32 +1,43 @@
 package com.example.android.topviewdarksky;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.topviewdarksky.models.CurrentWeather;
 import com.example.android.topviewdarksky.models.DailyWeatherData;
 import com.example.android.topviewdarksky.ui.WeatherAdapter;
 import com.example.android.topviewdarksky.ui.WeatherViewModel;
-import com.example.android.topviewdarksky.util.MyLocationListener;
 import com.example.android.topviewdarksky.util.WeatherIcons;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -58,19 +69,13 @@ public class MainActivity extends AppCompatActivity  {
     TextView currentTempTextView;
     RecyclerView dailyRecyclerView;
 
+    int PERMISSION_ID = 44;
+    FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        getlocation();
-
-//        latitude = location.getLatitude();
-//        longitude = location.getLongitude();
-
-        latitude = 40.7128;
-        longitude = -74.0060;
 
         sharedPreferences = getApplicationContext().getSharedPreferences(CITY_NAME_KEY,Context.MODE_PRIVATE);
 
@@ -79,6 +84,13 @@ public class MainActivity extends AppCompatActivity  {
         currentTempTextView = findViewById(R.id.temp_textview);
 
         dailyRecyclerView = findViewById(R.id.daily_recyclerview);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+
+//        latitude = 40.7128;
+//        longitude = -74.0060;
+
 
         if(latitude != null && longitude != null) {
             currentCityName = getCurrentCityName(latitude, longitude);
@@ -145,33 +157,111 @@ public class MainActivity extends AppCompatActivity  {
         return cityName;
     }
 
-    private void getlocation(){
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setCostAllowed(false);
-
-        String provider = locationManager.getBestProvider(criteria, false);
-        Location location = null;
-        try {
-            location = locationManager.getLastKnownLocation(provider);
-
-            MyLocationListener mylistener = new MyLocationListener();
-
-            if (location != null) {
-                // add location to the location listener for location changes
-                mylistener.onLocationChanged(location);
+    @SuppressLint("MissingPermission")
+    private void getLastLocation(){
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if (location == null) {
+                                    requestNewLocationData();
+                                } else {
+                                    latitude = location.getLatitude();
+                                    longitude = location.getLongitude();
+                                    Log.d("mlat", location.getLatitude()+"");
+                                    Log.d("mlon", location.getLongitude()+"");
+//                                    latTextView.setText(location.getLatitude()+"");
+//                                    lonTextView.setText(location.getLongitude()+"");
+                                }
+                            }
+                        }
+                );
             } else {
+                Toast.makeText(this, "Turn on location", Toast.LENGTH_LONG).show();
                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(intent);
             }
-
-            // location updates: at least 1 meter and 500 milli seconds change
-            locationManager.requestLocationUpdates(provider, 500, 1, mylistener);
-        } catch (SecurityException e) {
-            Log.e("SecurityException", e.getMessage());
+        } else {
+            requestPermissions();
         }
     }
+
+
+    @SuppressLint("MissingPermission")
+    private void requestNewLocationData(){
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
+
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            Log.d("mlastlat", mLastLocation.getLatitude()+"");
+            Log.d("mlastlon", mLastLocation.getLongitude()+"");
+//            latTextView.setText(mLastLocation.getLatitude()+"");
+//            lonTextView.setText(mLastLocation.getLongitude()+"");
+        }
+    };
+
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        return false;
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_ID
+        );
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            }
+        }
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        if (checkPermissions()) {
+            getLastLocation();
+        }
+
+    }
+
 }
 
 /*
