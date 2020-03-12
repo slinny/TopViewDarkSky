c102628cfeba710880c234ef6d0cfa71d08d5bd8
slinny <siranli411@gmail.com>
2020-03-12 10:30:22 -0400

    cleaned code

--- app/src/main/java/com/example/android/topviewdarksky/MainActivity.java
+++ app/src/main/java/com/example/android/topviewdarksky/MainActivity.java
@@ -1,34 +1,26 @@
 package com.example.android.topviewdarksky;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Looper;
-import android.provider.Settings;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.topviewdarksky.models.CurrentWeather;
 import com.example.android.topviewdarksky.models.DailyWeatherData;
@@ -36,21 +28,11 @@ import com.example.android.topviewdarksky.ui.WeatherAdapter;
 import com.example.android.topviewdarksky.ui.WeatherViewModel;
 import com.example.android.topviewdarksky.util.WeatherIcons;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-
-import static com.example.android.topviewdarksky.util.Constants.CITY_NAME_KEY;
-import static com.example.android.topviewdarksky.util.Constants.DEFAULT_CITY_NAME;
-import static com.example.android.topviewdarksky.util.Constants.SHARED_PREFS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -80,8 +62,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        sharedPreferences = getApplicationContext().getSharedPreferences(CITY_NAME_KEY, Context.MODE_PRIVATE);
 
         currentIconImageView = findViewById(R.id.current_icon_imageView);
         currentCityTextView = findViewById(R.id.city_textView);
@@ -139,9 +119,13 @@ public class MainActivity extends AppCompatActivity {
         weatherViewModel.getCurrentWeatherLiveData().observe(this, new Observer<CurrentWeather>() {
             @Override
             public void onChanged(@Nullable final CurrentWeather currentWeather) {
-                mCurrentWeather = currentWeather;
-                setCurrentIcon(mCurrentWeather.getIcon());
-                currentTempTextView.setText(setCurrentTemp(mCurrentWeather.getTemperature()));
+                try {
+                    mCurrentWeather = currentWeather;
+                    setCurrentIcon(mCurrentWeather.getIcon());
+                    currentTempTextView.setText(setCurrentTemp(mCurrentWeather.getTemperature()));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -180,14 +164,13 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         String cityName = addresses.get(0).getLocality();
-//        Log.d("mainCityName", cityName);
         return cityName;
     }
 }
 
 /*
 1. databinding
-2. unit and UI testing
+2. unit and UI test
 5. dagger2
 6. rxjava
  */
